82d0bd5a72fa93c18034fac27c61d9c2e9bf047b
JGriff07 <jonathan.griffin.07@gmail.com>
2023-08-06 19:58:26 -0700

    10th Commit - Fix Default Values to plot placeholder Gantt Chart

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ st.header("Input the Vul Start and KIO Times")
 
 start_df = pd.DataFrame(
     [
-        {"Vul Start":"12:00:00", "Knock It Off": "12:00:00"}
+        {"Vul Start":"12:00:00", "Knock It Off": "13:00:00"}
     ]
 )
 
@@ -25,9 +25,9 @@ st.header("Input :blue[Blue Timeline] Here (9-Lines)")
 
 blue_df = pd.DataFrame(
     [
-        {"Label": "Alpha", "From": "XR99", "To": "Hog", "Nominated": "12:00:00", "Passed": "12:00:00", "First Effect": "12:00:00", "Last Effect": "12:00:00"},
-        {"Label": "Bravo", "From": "XR99", "To": "Hog", "Nominated": "12:00:00", "Passed": "12:00:00", "First Effect": "12:00:00", "Last Effect": "12:00:00"},
-        {"Label": "Charlie", "From": "XR99", "To": "Hog", "Nominated": "12:00:00", "Passed": "12:00:00", "First Effect": "12:00:00", "Last Effect": "12:00:00"}
+        {"Label": "Alpha", "From": "XR99", "To": "Hog", "Nominated": "12:01:00", "Passed": "12:03:00", "First Effect": "12:05:00", "Last Effect": "12:10:00"},
+        {"Label": "Bravo", "From": "XR99", "To": "Hog", "Nominated": "12:11:00", "Passed": "12:13:00", "First Effect": "12:15:00", "Last Effect": "12:20:00"},
+        {"Label": "Charlie", "From": "XR99", "To": "Hog", "Nominated": "12:21:00", "Passed": "12:23:00", "First Effect": "12:25:00", "Last Effect": "12:30:00"}
     ]
 )
 
@@ -38,7 +38,7 @@ st.header("Input :red[Red Timeline] Here (IDF)")
 
 red_df = pd.DataFrame(
     [
-        {"Label": "1st IDF", "Start": "12:00:00", "End": "12:00:00"}
+        {"Label": "1st IDF", "Start": "12:10:00", "End": "12:20:00"}
     ]
 )
 
