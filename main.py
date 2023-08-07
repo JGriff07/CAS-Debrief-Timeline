6deda4ff297e256102455fd178c71821c63429c0
JGriff07 <jonathan.griffin.07@gmail.com>
2023-08-06 19:30:57 -0700

    4th Commit - Removed static DataFrame, and added data_editor with dynamic rows

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import numpy as np
 
 df = pd.DataFrame(
     [
-        {"Label": "Alpha", "From": "XR99", "To": "Hog", "Nominated": 12:00:00},
-        {"Label": "Bravo", "From": "XR99", "To": "Hog", "Nominated": 12:00:00},
-        {"Label": "Charlie", "From": "XR99", "To": "Hog", "Nominated": 12:00:00}
+        {"Label": "Alpha", "From": "XR99", "To": "Hog", "Nominated": "12:00:00"},
+        {"Label": "Bravo", "From": "XR99", "To": "Hog", "Nominated": "12:00:00"},
+        {"Label": "Charlie", "From": "XR99", "To": "Hog", "Nominated": "12:00:00"}
     ]
 )
 
-edited_df = st.data_editor(dr, num_rows="dynamic")
+edited_df = st.data_editor(df, num_rows="dynamic")
 
