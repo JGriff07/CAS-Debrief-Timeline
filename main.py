f521929f91b41e2f8853366836bfce74a2ae2a95
JGriff07 <jonathan.griffin.07@gmail.com>
2023-08-06 19:20:36 -0700

    2nd Commit - Import streamlit, pandas, and numpy; make first dataframe

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 #Connect PyCharm to GitHub Repo
 #Connect GitHub to Streamlit, Host on Streamlit
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+blank_array = np.array([[1,2,3], [4,5,6], [7,8,9]], columns=['a', 'b', 'c'])
+blank_dataframe = pd.DataFrame(blank_array)
+
+st.dataframe(data=blank_dataframe)
+
